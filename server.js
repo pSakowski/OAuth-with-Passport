c1601ea8e0965db7c90565216c010971eebf470c
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-03-13 20:50:33 +0100

    add authenticate endpoints & passport.use middleware

--- server.js
+++ server.js
@@ -5,8 +5,27 @@ const hbs = require('express-handlebars');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const { profile } = require('console');
 
 const app = express();
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.clientID,
+  clientSecret: process.env.clientSecret,
+  callbackURL: process.env.callbackURL,
+}, (accessToken, refreshToken, profile, done) => {
+done(null, profile);
+}));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -15,6 +34,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
+
+// app.get('/auth/google/callback', (req, res) => {
+//   res.send(`I'm back from Google!`);
+// });
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -31,6 +68,7 @@ app.get('/user/no-permission', (req, res) => {
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
 });
+
 
 app.listen('8000', () => {
   console.log('Server is running on port: 8000');
